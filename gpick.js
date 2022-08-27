153007b790d8be02f8f8e513fef75362f8dca735
David Konsumer <konsumer@jetboystudio.com>
2022-08-27 13:26:27 -0700

    trying to get the multi-pick UI working

--- gpick.js
+++ gpick.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 const git = require('isomorphic-git')
 const fs = require('fs')
 const dir = process.cwd()
@@ -54,7 +56,7 @@ async function main () {
 
   if (branchFrom === branchTo) {
     y.showHelp()
-    console.error(`\nYou are trying to pick from '${branchFrom}' to itself.`)
+    console.error(`\nYou are trying to pick from '${branchFrom}' to itself. Use one of these: ${branches.filter(b => b !== branchFrom).join(', ')}`)
     process.exit(1)
   }
 
@@ -70,17 +72,57 @@ async function main () {
     screen.title = `gpick ${branchFrom} ${branchTo}`
     screen.key(['escape', 'q', 'C-c'], (ch, key) => process.exit(0))
 
-    const list = blessed.list({
-      height: '100%',
-      width: '100%',
-      interactive: true,
-      invertSelected: true,
+    const choices = new Set()
 
+    const list = blessed.list({
+      width: '100%',
+      invertSelected: false,
+      style: {
+        selected: { bg: 'white', fg: 'black' }
+      },
+      keys: true,
+      mouse: true,
       tags: true,
-      items: commitsDiff.map(c => `{red-fg}${c.oid}{/} - ${c.message} (${c.when}) <${c.author}>`)
+      items: commitsDiff.map((c, i) => `${choices.has(i) ? '*' : ' '} {red-fg}${c.oid}{/red-fg} - ${c.message} {green-fg}(${c.when}){/green-fg} {blue-fg}<${c.author}>{/blue-fg}`)
     })
     list.focus()
     screen.append(list)
+
+    // list.key(['space'], () => {
+    //   choices.has(list.selected)
+    //     ? choices.delete(list.selected)
+    //     : choices.add(list.selected)
+    //   const c = commitsDiff[list.selected]
+    //   // list.setLine(list.selected, `${choices.has(list.selected) ? '*' : ' '} {red-fg}${c.oid}{/red-fg} - ${c.message} {green-fg}(${c.when}){/green-fg} {blue-fg}<${c.author}>{/blue-fg}`)
+    //   list.setLine(list.selected, choices.has(list.selected) ? '*' : ' ')
+    //   screen.render()
+    // })
+
+    list.on('select', () => {
+      choices.has(list.selected)
+        ? choices.delete(list.selected)
+        : choices.add(list.selected)
+      const c = commitsDiff[list.selected]
+      // hmm, not working
+      list.setLine(list.selected, `${choices.has(list.selected) ? '*' : ' '} {red-fg}${c.oid}{/red-fg} - ${c.message} {green-fg}(${c.when}){/green-fg} {blue-fg}<${c.author}>{/blue-fg}`)
+      list.render()
+      screen.render()
+      console.log(list.selected)
+    })
+
+    const infobar = blessed.box({
+      width: '100%',
+      height: 1,
+      bottom: 0,
+      tags: true,
+      style: {
+        fg: 'black',
+        bg: 'grey'
+      },
+      content: '{center}Use esc to quit, arrows to navigate, enter to choose commits, space to cherry-pick.{/center}'
+    })
+    screen.append(infobar)
+
     screen.render()
   }
 }
